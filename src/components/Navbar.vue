62ea3b3f596958032a4a21e65cc6d1f786a0f574
Isis Lázaro <isislazaro@gmail.com>
2019-10-30 10:21:43 -0600

    add vuetify, remove some vuetify intial settings, add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title>
+      <p>together-click</p>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
+  <v-content>
     <h1>This is an about page</h1>
-  </div>
+  </v-content>
 </template>
+<script>
+// @ is an alias to /src
+export default {
+  name: "about"
+};
+</script>
