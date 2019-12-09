8070d24a18c4aaf05874f660da55b2a7b35ed149
Isis Lázaro <isislazaro@gmail.com>
2019-12-08 20:22:44 -0600

    login module

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,30 @@
 <template>
   <v-content>
-    <h1>This is an about page</h1>
+    <v-card width="500" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">This is an about page</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="Nombre de usuario"
+            prepend-icon="mdi-account-circle"
+          />
+          <v-text-field
+            type="password"
+            label="Contraseña"
+            prepend-icon="mdi-lock"
+            append-icon="mdi-eye-off"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Regístrate</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Conéctate</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-content>
 </template>
 <script>
